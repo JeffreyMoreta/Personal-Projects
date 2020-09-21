9e54a5a6b3d88b6533465f881a3dc70e6d62054c
JeffreyMoreta <jeffreymoreta@gmail.com>
2020-09-21 09:40:13 -0400

    Delete database.js

--- savage-auth/config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb+srv://rcAdmin:test1234@rcclassroom.qgez6.mongodb.net/rcClassroom?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    'dbName': 'rcClassroom'
-};
